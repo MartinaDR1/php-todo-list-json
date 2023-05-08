9ac9a4dedfb99a475c0d71d3deb9f805d505517e
MartinaDR1 <derosemartina933@gmail.com>
2023-05-08 14:35:53 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- My css -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>PHP ToDo List JSON</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center my-3">Todo List</h1>
+    </div>
+
+
+
+    <!-- Axios -->
+    <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
+
+    <!-- Vue -->
+    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+
+    <!-- My js -->
+    <script src='./app.js'></script>
+</body>
+</html>
